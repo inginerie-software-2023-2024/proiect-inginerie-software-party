f703afc10caca14c2d9fb3553da563662a86791c
CristinaBoboc20 <cristinaboboc26@yahoo.com>
2024-01-30 13:55:05 +0200

    added unit tests

---
+++ PetConnect/PetConnectTest/BlogControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PetConnect.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetConnectTest
+{
+    public class BlogControllerTests
+    {
+        [Test]
+        public void Blog1_ReturnsViewResult()
+        {
+            // Arrange
+            var controller = new BlogsController();
+
+            // Act
+            var result = controller.Blog1() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Blog2_ReturnsViewResult()
+        {
+            // Arrange
+            var controller = new BlogsController();
+
+            // Act
+            var result = controller.Blog2() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Blog3_ReturnsViewResult()
+        {
+            // Arrange
+            var controller = new BlogsController();
+
+            // Act
+            var result = controller.Blog3() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
